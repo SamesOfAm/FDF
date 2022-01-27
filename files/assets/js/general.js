71b0f348ae97e3d934c5a10c74fb629edadf4994
Florian <flo.tepelmann@ewetel.net>
2022-01-27 21:28:01 +0100

    Änderungen

--- files/assets/js/general.js
+++ files/assets/js/general.js
@@ -22,6 +22,20 @@ function parallaxY(scrollingOffsets) {
     }
 }
 
+function parallaxYSlow(scrollingOffsets) {
+    for(let i = 0; i < allScrollingObjects.length; i++) {
+        /* allScrollingObjects[i].animate({
+            top: (scrollingOffsets[i] + allScrollingObjects[i].offsetHeight/2) - (window.pageYOffset + window.innerHeight/2).toString() + "px"
+        },
+            1000,
+            'swing',
+            function() {
+                console.log('Finished');
+            }); */
+        allScrollingObjects[i].style.top = ((scrollingOffsets[i] + allScrollingObjects[i].offsetHeight/2) - (window.pageYOffset + window.innerHeight/2)/2).toString() + "px";
+    }
+}
+
 function parallaxXLeft(scrollingOffsets) {
     for(let i = 0; i < allSphereObjectsLeft.length; i++) {
         allSphereObjectsLeft[i].querySelector('figure').style.left = (((scrollingOffsets[i] + allSphereObjectsLeft[i].offsetHeight/2) - (window.pageYOffset + window.innerHeight/2).toString())/2) + "px";
@@ -29,12 +43,13 @@ function parallaxXLeft(scrollingOffsets) {
 }
 
 function parallaxXRight(scrollingOffsets) {
-    for(let i = 0; i < allSphereObjectsLeft.length; i++) {
-        allSphereObjectsRight[i].querySelector('figure').style.right = (((scrollingOffsets[i] + allSphereObjectsLeft[i].offsetHeight/2) - (window.pageYOffset + window.innerHeight/2).toString())/2) + "px";
+    for(let i = 0; i < allSphereObjectsRight.length; i++) {
+        allSphereObjectsRight[i].querySelector('figure').style.right = (((scrollingOffsets[i] + allSphereObjectsRight[i].offsetHeight/2) - (window.pageYOffset + window.innerHeight/2).toString())/2) + "px";
     }
 }
 
 let allScrollingOffsets = [];
+let allSlowScrollingOffsets = [];
 let allSphereOffsetsLeft = [];
 let allSphereOffsetsRight = [];
 
@@ -42,6 +57,12 @@ const allScrollingObjects = document.querySelectorAll('.scrolling');
 allScrollingObjects.forEach(object => {
     const originalYOffset = getPosition(object).offsetTop;
     allScrollingOffsets.push(originalYOffset);
+})
+
+const allSlowScrollingObjects = document.querySelectorAll('.scrolling.langsam');
+allSlowScrollingObjects.forEach(object => {
+    const originalYOffset = getPosition(object).offsetTop;
+    allSlowScrollingOffsets.push(originalYOffset);
 })
 
 const allSphereObjectsLeft = document.querySelectorAll('.scrolling-sphere-left');
@@ -70,10 +91,36 @@ const fixHeader = () => {
     }
 }
 
-window.addEventListener("scroll", function(){
-    parallaxY(allScrollingOffsets);
-    parallaxXLeft(allSphereOffsetsLeft);
-    parallaxXRight(allSphereOffsetsRight);
-    footerEffect();
-    fixHeader();
+console.log(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
+
+if( window.innerWidth >= 1025 ) {
+    document.addEventListener("DOMContentLoaded", function (event) {
+        window.addEventListener("scroll", function () {
+            parallaxY(allScrollingOffsets);
+            parallaxYSlow(allSlowScrollingOffsets);
+            parallaxXLeft(allSphereOffsetsLeft);
+            parallaxXRight(allSphereOffsetsRight);
+            footerEffect();
+            fixHeader();
+        });
+        window.scrollTo(0, 1);
+        window.scrollTo(0, 0);
+    });
+}
+
+
+
+jQuery('.to-top-button').hide();
+jQuery(window).scroll(function(){
+    const value=400;
+    const scrolling = jQuery(window).scrollTop();
+    if(scrolling>value){
+        jQuery('.to-top-button').fadeIn();
+    } else{
+        jQuery('.to-top-button').fadeOut();
+    }
+});
+jQuery('.to-top-button').click(function(){
+    jQuery('html, body').animate({scrollTop:'0px'},800);
+    return !1;
 });
