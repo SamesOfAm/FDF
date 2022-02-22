7a3d84d02dc849ec5b75d2deb575aaeeeaa1b2c4
SamesOfAm <flo.tepelmann@ewetel.net>
2022-02-22 16:54:11 +0100

    fe_page (Test three.js 360 video)

---
+++ templates/home_video.php
@@ -0,0 +1,5 @@
+<div id="container"><canvas data-engine="three.js r137"></canvas></div>
+
+<video id="video" loop="" muted="" crossorigin="anonymous" playsinline="" style="display:none">
+  <source src="files/videos/pano.webm">
+</video>
